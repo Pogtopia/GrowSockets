aa68a464a96fcdece163b01f5d4ddc1c1635326b
Alexander9673 <alexander.m9673@gmail.com>
2020-11-11 13:57:47 +0800

    Add more methods natively.

--- native/main.cc
+++ native/main.cc
@@ -3,19 +3,23 @@
 #include <unordered_map>
 
 #define ARG Napi::CallbackInfo& info
-std::unordered_map<uint32_t, ENetPeer*> peers;
 
+std::unordered_map<uint32_t, ENetPeer*> peers;
 ENetHost* host;
+Napi::FunctionReference emitter;
+
+uint32_t netID = 0;
 
 void __init(ARG) {
   Napi::Env env = info.Env();
+  uint32_t port = info[0].ToNumber().Uint32Value();
 
   if (enet_initialize() != 0)
     return Napi::Error::New(env, "ENet failed to Initialize.").ThrowAsJavaScriptException();
 
   ENetAddress address;
   address.host = ENET_HOST_ANY;
-  address.port = 17091;
+  address.port = static_cast<uint16_t>(port);
 
   host = enet_host_create(&address, 1024, 2, 0, 0);
   host->checksum = enet_crc32;
@@ -42,9 +46,86 @@ void __send(ARG) {
   }
 }
 
+void __accept(ARG) {
+  Napi::Env env = info.Env();
+
+  if (!emitter)
+    return Napi::Error::New(env, "Can't watch for events without the emit function.").ThrowAsJavaScriptException();
+
+  ENetEvent event;
+
+  if (enet_host_service(host, &event, 0) > 0)
+    switch (event.type) {
+      uint32_t lastNetID = netID++;
+      event.peer->data   = new uint8_t[sizeof(uint32_t)];
+
+      *(uint32_t*)event.peer->data = lastNetID;
+
+      case ENET_EVENT_TYPE_CONNECT: {
+        emitter.Call({
+          Napi::String::New(env, "connect"),
+          Napi::Number::New(env, lastNetID)
+        });
+        break;
+      }
+
+      case ENET_EVENT_TYPE_RECEIVE: {
+        auto packet = new unsigned char[event.packet->dataLength];
+        memcpy(packet, event.packet->data, event.packet->dataLength);
+
+        emitter.Call({
+          Napi::String::New(env, "data"),
+          Napi::Number::New(env, *(int*)event.peer->data),
+          Napi::Buffer<uint8_t>::New(env,
+                                    packet,
+                                    event.packet->dataLength,
+                                    [](Napi::Env env, uint8_t* data)
+          {
+            delete[] data;
+          })
+        });
+
+        enet_packet_destroy(event.packet);
+        break;
+      }
+
+      case ENET_EVENT_TYPE_DISCONNECT: {
+        uint32_t userNetID = *(uint32_t*)event.peer->data;
+        
+        emitter.Call({
+          Napi::String::New(env, "disconnect"),
+          Napi::Number::New(env, userNetID)
+        });
+
+        if (peers[userNetID])
+          peers.erase(userNetID);
+
+        delete[] event.peer->data;
+
+        break;
+      }
+    }
+}
+
+void __set_netID(ARG) {
+  netID = info[0].ToNumber().Uint32Value();
+}
+
+void __close(ARG) {
+  enet_deinitialize();
+}
+
+void __set_emitter(ARG) {
+  emitter = Napi::Persistent(info[0].As<Napi::Function>());
+}
+
 Napi::Object __reg(Napi::Env env, Napi::Object exports) {
-  exports["init"] = Napi::Function::New(env, __init);
-  exports["send"] = Napi::Function::New(env, __send);
+  exports["init"]     = Napi::Function::New(env, __init);
+  exports["send"]     = Napi::Function::New(env, __send);
+  exports["accept"]   = Napi::Function::New(env, __accept);
+  exports["setNetID"] = Napi::Function::New(env, __set_netID);
+  exports["deInit"]   = Napi::Function::New(env, __close);
+  exports["emitter"]  = Napi::Function::New(env, __set_emitter);
 
   return exports;
 }
--- src/core/Wrapper.ts
+++ src/core/Wrapper.ts
@@ -19,6 +19,27 @@ const Wrapper = {
    */
   send: (id: number, count: number, ...packets: Buffer[]): void =>
     native.send(id, count, packets),
+
+  /**
+   * Accepts incoming data/connections by calling "enet_host_service". This should be called in a loop.
+   */
+  accept: (): void => native.accept(),
+
+  /**
+   * Sets the netID to use. Only run this once, if not ran, it will start at 0.
+   * @param netID The netID to set it to.
+   */
+  setNetID: (netID: number): void => native.setNetID(netID),
+
+  /**
+   * De-initializes ENet.
+   */
+  deInit: () => native.deInit(),
+
+  /**
+   * Set the emitter required by the server to emit events.
+   */
+  emitter: (emit: (...args: any[]) => any[]) => native.emitter(emit),
 };
 
 export default Wrapper;
