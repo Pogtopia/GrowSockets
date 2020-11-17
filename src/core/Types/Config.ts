2a7bc875d1abcd9045a75b7189060b956779ca32
Alexander9673 <alexander.m9673@gmail.com>
2020-11-17 21:57:21 +0800

    Create Config.ts

---
+++ src/core/Types/Config.ts
@@ -0,0 +1,39 @@
+/**
+ * Options for the http server.
+ */
+export interface HTTPOptions {
+  /**
+   * Whether or not to use the built-in HTTP Server. Defaults to `false`
+   */
+  enabled?: boolean;
+
+  /**
+   * The port of the server. Defaults to `17091`
+   */
+  serverPort?: number;
+
+  /**
+   * The ip of the server. Defaults to `127.0.0.1`
+   */
+  serverIP?: string;
+}
+
+/**
+ * The configuration for the server.
+ */
+export interface Config {
+  /**
+   * The port to use for the server. Defaults to `17091`
+   */
+  port?: number;
+
+  /**
+   * The options for the HTTP server.
+   */
+  http?: HTTPOptions;
+
+  /**
+   * The cache manager to use.
+   */
+  cache?: any;
+}
